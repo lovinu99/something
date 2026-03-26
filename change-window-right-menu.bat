7ca522b356c7b249937fccf01f9b3bd821fb1465
danh <nhdanh2604@gmail.com>
2026-03-26 17:18:39 +0700

    feat

--- change-window-right-menu.bat
+++ change-window-right-menu.bat
@@ -59,6 +59,10 @@ echo Restoring Modern Context Menu...
 reg.exe delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
 
 echo Done.
-echo You may need to restart Explorer or reboot.
+echo Restarting explorer...
+
+taskkill /f /im explorer.exe
+start explorer.exe
+
 pause
 goto menu
