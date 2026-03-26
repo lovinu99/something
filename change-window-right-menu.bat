f09be2ecf40c32c966e79d79a3e7b1b51518f98d
danh <nhdanh2604@gmail.com>
2026-03-26 17:21:26 +0700

    feaet

--- change-window-right-menu.bat
+++ change-window-right-menu.bat
@@ -45,7 +45,10 @@ echo Applying Classic Context Menu...
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 
 echo Done.
-echo You may need to restart Explorer or reboot.
+echo Restarting explorer...
+
+taskkill /f /im explorer.exe
+start explorer.exe
 pause
 goto menu
 
